0c10323894ae14d8a3afced0686add3ffdbcd7a7
gregoireherbin <melozy@free.fr>
2025-07-06 23:56:10 +0200

    test erreur console.log

--- lib/initSupabase.ts
+++ lib/initSupabase.ts
@@ -1,5 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
+if (typeof window !== 'undefined') {
+  console.log('🌐 NEXT_PUBLIC_SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
+  console.log('🔑 NEXT_PUBLIC_SUPABASE_ANON_KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+}
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
